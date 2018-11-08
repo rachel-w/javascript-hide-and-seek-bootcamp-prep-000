a2e8509784aef45b87945e604a6234230f1a1a2a
Rachel Wey <rachelwey96@outlook.com>
2018-11-08 08:18:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ function nestedTarget(){
     return document.getElementById("nested").querySelector(".target")
 }
 
-function increaseRankBy(n){
-
-}
-
 function deepestChild(){
+  var li = document.getElementById("grand-node").querySelectorAll("div")
+  var child
 
+  for (let i=0; i < li.length; i++){
+    child = li[i].querySelector("div")
+  }
+  return child
 }
