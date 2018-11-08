913c5146d71de14ad57cdcce033a6267e603df59
Rachel Wey <rachelwey96@outlook.com>
2018-11-08 08:24:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy(n){
   var list = document.getElementById("app").querySelectorAll("ul.ranked-list li")
 
   for (let i = 0; i <list.length; i++){
-    list[i].innerHTML = parseInt(i + 1)
+    list[i].innerHTML = parseInt(list[i].innerHTML + n)
   }
 }
 
