0f724e66484cb55b2c53d11265a0ab45ad2a1789
Rachel Wey <rachelwey96@outlook.com>
2018-11-08 08:27:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,11 +19,10 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  let node = document.getElementById('grand-node')
-  let nextNode = node.children[0]
-
-  while (nextNode) {
-    node = nextNode
-    nextNode = node.children[0]
-  }
+  var grandNode = document.querySelector('#grand-node');
+    var deepestNode = grandNode.children[0];
+    for (var i = 0; !deepestNode.children[i]; i) {
+      deepestNode = deepestNode.children[0]
+    }
+    return deepestNode;
 }
